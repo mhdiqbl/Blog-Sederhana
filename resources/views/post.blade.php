df775ca8665a97b28da4f08aa416c7996564b5d1
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-03-16 02:31:34 +0700

    Learn Seeder

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,9 @@
 
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
-    <p>By. Iqbal in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="">{{ $post->user->name }}</a> in <a
+            href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 
-    <a href=" /posts">Back to Blog</a>
+    <a href=" /posts" class="d-block mt-3">Back to Blog</a>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,8 @@
             <h2>
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
-            <p>By. Iqbal in <a href="/categories/{{ $post->category->slug }}"
+            <p>By. <a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a
+                    href="/categories/{{ $post->category->slug }}"
                     class="text-decoration-none">{{ $post->category->name }}</a></p>
             <p>{{ $post->excerpt }}</p>
 
