014f175cbf1d17d8b9bf3518eca2f419546b0aaf
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-03-20 21:58:20 +0700

    Learn Factory & Faker

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,9 @@
 
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
-    <p>By. <a href="">{{ $post->user->name }}</a> in <a
-            href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in
+        <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
+    </p>
     {!! $post->body !!}
 
     <a href=" /posts" class="d-block mt-3">Back to Blog</a>
