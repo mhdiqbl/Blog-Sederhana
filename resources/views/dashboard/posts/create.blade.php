e883b6668d2f98b0dcc3b4c542dba13ff0463728
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-04-02 15:17:39 +0700

    Add Trix Editor

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -23,6 +23,11 @@
                     @endforeach
                 </select>
             </div>
+            <div class="mb-3">
+                <label for="body" class="form-label">Body</label>
+                <input type="hidden" id="body" name="body">
+                <trix-editor input="body"></trix-editor>
+            </div>
 
             <button type="submit" class="btn btn-primary">Create Post</button>
         </form>
@@ -36,6 +41,10 @@
             fetch('/dashboard/posts/checkSlug?title=' + title.value)
                 .then(response => response.json())
                 .then(data => slug.value = data.slug)
+        });
+
+        document.addEventListener('trix-file-accept', function(e) {
+            e.preventDefault();
         })
     </script>
 @endsection
