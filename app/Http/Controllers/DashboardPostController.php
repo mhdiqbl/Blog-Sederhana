7781999039fa8e548f084fbe98ce3ca5ce3f0381
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-04-04 01:32:18 +0700

    Add Update & Delete Image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 
@@ -53,9 +54,9 @@ class DashboardPostController extends Controller
             'body' => 'required',
         ]);
 
-         if($request->file('image')){
+        if($request->file('image')){
              $validatedData['image'] = $request->file('image')->store('post-images');
-         }
+        }
 
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -104,6 +105,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required',
         ];
         if($request->slug != $post->slug){
@@ -111,6 +113,13 @@ class DashboardPostController extends Controller
         }
 
         $validatedData = $request->validate($rules);
+
+        if($request->file('image')){
+            if($request->oldImage){
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -128,7 +137,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-
+        if($post->image){
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
         return redirect('/dashboard/posts')->with('success', 'Post has been added!');
     }
