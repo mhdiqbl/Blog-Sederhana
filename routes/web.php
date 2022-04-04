01d19177c15c58ac27e6fa7014bdd178b7959e46
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-04-04 17:22:37 +0700

    Add middleware IsAdmin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\DashboardPostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
@@ -68,6 +69,7 @@ Route::get('/dashboard', function(){
 Route::get('/dashboard/posts/checkSlug',[DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
 
 // Route::get('/categories/{category:slug}', function(Category $category){
 //     return view('posts',[
